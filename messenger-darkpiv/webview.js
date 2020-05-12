b1cd63c12894f8aeb33b81c8d99fc00975e7ef2f
darkpiv <duynhat.cse@gmail.com>
2020-05-12 21:47:49 +0700

    add messenger

---
+++ messenger-darkpiv/webview.js
@@ -0,0 +1,35 @@
+"use strict";
+const path = require('path');
+
+module.exports = Franz => {
+  const getMessages = function getMessages() {
+    let count = document.querySelectorAll('._5fx8:not(._569x),._1ht3:not(._569x)').length;
+    const messageRequestsElement = document.querySelector('._5nxf');
+
+    if (messageRequestsElement) {
+      count += parseInt(messageRequestsElement.innerHTML, 10);
+    }
+
+    Franz.setBadge(count);
+  };
+  Franz.injectCSS(path.join(__dirname, 'style.css'));
+  Franz.loop(getMessages);
+  localStorage.setItem('_cs_desktopNotifsEnabled', JSON.stringify({
+    __t: new Date().getTime(),
+    __v: true
+  }));
+
+  if (typeof Franz.onNotify === 'function') {
+    Franz.onNotify(notification => {
+      if (typeof notification.title !== 'string') {
+        notification.title = ((notification.title.props || {}).content || [])[0] || 'Messenger';
+      }
+
+      if (typeof notification.options.body !== 'string') {
+        notification.options.body = (((notification.options.body || {}).props || {}).content || [])[0] || '';
+      }
+
+      return notification;
+    });
+  }
+};
